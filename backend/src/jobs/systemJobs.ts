6350866a84dad88e46330c3ce7c14c98e7e5460c
towfiqi <tislam100@gmail.com>
2026-02-08 18:35:35 +0600

    fix: download cleaning cron task

--- backend/src/jobs/systemJobs.ts
+++ backend/src/jobs/systemJobs.ts
@@ -1,12 +1,12 @@
 import { SystemJobConfig } from '../types/global';
 
 export const SYSTEM_JOBS: SystemJobConfig[] = [
-	// {
-	// 	name: 'CleanDownloads',
-	// 	schedule: '0 2 * * *', // Every day at 2:00 AM
-	// 	maxAttempts: 2,
-	// 	retryDelay: 300000,
-	// },
+	{
+		name: 'CleanDownloads',
+		schedule: '0 2 * * *', // Every day at 2:00 AM
+		maxAttempts: 2,
+		retryDelay: 300000,
+	},
 	// {
 	// 	name: 'PruneDatabase',
 	// 	schedule: '0 3 * * *', // Every day at 3:00 AM
